80300f4dceb25da03660604b1e0a37cb6f599e65
Chalermrid Patipanang <chalermrid.p@gmail.com>
2021-01-30 18:59:34 +0700

    init results module, service, controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ResultsModule } from './results/results.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ResultsModule],
 })
 export class AppModule {}
